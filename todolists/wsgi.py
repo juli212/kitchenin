56f4951f197b9319d355ca5e3acc5c9898c60189
juli212 <jjgiraldi@gmail.com>
2017-12-05 16:45:09 -0500

    undo

--- todolists/wsgi.py
+++ todolists/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todolists.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "todolists.settings")
 
 application = get_wsgi_application()
