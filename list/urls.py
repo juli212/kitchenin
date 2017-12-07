c908384f4fc8b1770b60fda18817eb609ebdbf81
juli212 <jjgiraldi@gmail.com>
2017-12-07 17:41:50 -0500

    move kitchen create to index route

--- list/urls.py
+++ list/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^(?P<list_id>[0-9]+)/$', views.detail, name='detail'),
 	url(r'^(?P<list_id>[0-9]+)/add_item/$', views.add_item, name='add_item'),
-	url(r'^create/$', views.create, name='create'),
 	url(r'^(?P<list_id>[0-9]+)/members/$', views.members, name='members'),
 	url(r'^(?P<list_id>[0-9]+)/find_member/$', views.find_member, name='find_member'),
 	url(r'^(?P<list_id>[0-9]+)/add_member/$', views.add_member, name='add_member'),
